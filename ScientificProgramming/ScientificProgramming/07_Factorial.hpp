2ad7d022d6406e7e60c4c0e2a54e9d82bbd7d721
Oscar Alejandro Hernandez Lopez <oscarale930719@gmail.com>
2020-10-20 06:18:14 -0500

    Create 07_Factorial.hpp

---
+++ ScientificProgramming/ScientificProgramming/07_Factorial.hpp
@@ -0,0 +1,17 @@
+//
+//  07_Factorial.hpp
+//  ScientificProgramming
+//
+//  Created by Oscar Alejandro Hernández López on 14/09/20.
+//  Copyright © 2020 Oscar Alejandro Hernández López. All rights reserved.
+//
+
+#ifndef _7_Factorial_hpp
+#define _7_Factorial_hpp
+
+#include <stdio.h>
+#include "Header.h"
+
+void factorial ();
+
+#endif /* _7_Factorial_hpp */
