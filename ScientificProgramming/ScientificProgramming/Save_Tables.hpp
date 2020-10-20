441f03792da59565c529eeef67b8e3fcadf8a91c
Oscar Alejandro Hernandez Lopez <oscarale930719@gmail.com>
2020-10-20 06:18:37 -0500

    Create Save_Tables.hpp

---
+++ ScientificProgramming/ScientificProgramming/Save_Tables.hpp
@@ -0,0 +1,17 @@
+//
+//  Save_Tables.hpp
+//  ScientificProgramming
+//
+//  Created by Oscar Alejandro Hernández López on 25/09/20.
+//  Copyright © 2020 Oscar Alejandro Hernández López. All rights reserved.
+//
+
+#ifndef Save_Tables_hpp
+#define Save_Tables_hpp
+
+#include <stdio.h>
+
+void mainSaveTable (int argc, const char *argv[]);
+struct table_characteristics* Generate(int N, int K);
+
+#endif /* Save_Tables_hpp */
