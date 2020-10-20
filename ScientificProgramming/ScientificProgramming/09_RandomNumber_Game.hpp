1e150794209b8639b37a575f25cdbeb1e359c96d
Oscar Alejandro Hernandez Lopez <oscarale930719@gmail.com>
2020-10-20 06:18:25 -0500

    Create 09_RandomNumber_Game.hpp

---
+++ ScientificProgramming/ScientificProgramming/09_RandomNumber_Game.hpp
@@ -0,0 +1,17 @@
+//
+//  09_RandomNumber_Game.hpp
+//  ScientificProgramming
+//
+//  Created by Oscar Alejandro Hernández López on 17/09/20.
+//  Copyright © 2020 Oscar Alejandro Hernández López. All rights reserved.
+//
+
+#ifndef _9_RandomNumber_Game_hpp
+#define _9_RandomNumber_Game_hpp
+
+#include <stdio.h>
+#include "Header.h"
+
+void randomNumber ();
+
+#endif /* _9_RandomNumber_Game_hpp */
