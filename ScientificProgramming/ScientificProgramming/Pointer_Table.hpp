39d349b79325707b3b6bdb9450e8bb30bb693e4c
Oscar Alejandro Hernandez Lopez <oscarale930719@gmail.com>
2020-10-20 06:18:34 -0500

    Create Pointer_Table.hpp

---
+++ ScientificProgramming/ScientificProgramming/Pointer_Table.hpp
@@ -0,0 +1,18 @@
+//
+//  Pointer_Table.hpp
+//  ScientificProgramming
+//
+//  Created by Oscar Alejandro Hernández López on 17/09/20.
+//  Copyright © 2020 Oscar Alejandro Hernández López. All rights reserved.
+//
+
+#ifndef Pointer_Table_hpp
+#define Pointer_Table_hpp
+
+#include <stdio.h>
+#include "Header.h"
+
+void mainPointerTable ();
+void fill(int sizeN, int quantityK, float **pointerMatrix);
+
+#endif /* Pointer_Table_hpp */
