0c5249022b2ebd80d06f612f87112e1d46cfb0f0
Oscar Alejandro Hernandez Lopez <oscarale930719@gmail.com>
2020-10-20 06:50:25 -0500

    Create Header.hpp

---
+++ TSP/Header.hpp
@@ -0,0 +1,26 @@
+//
+//  Header.hpp
+//  TSP
+//
+//
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#include <list>
+#include <set>
+#include <assert.h>
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+
+
+#endif /* Header_h */
