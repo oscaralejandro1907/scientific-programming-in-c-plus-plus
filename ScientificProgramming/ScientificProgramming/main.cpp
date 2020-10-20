ffba4a035ae9090804f61f2b00281655b07857b6
Oscar Alejandro Hernandez Lopez <oscarale930719@gmail.com>
2020-10-20 06:18:46 -0500

    Create main.cpp

---
+++ ScientificProgramming/ScientificProgramming/main.cpp
@@ -0,0 +1,49 @@
+//
+//  main.cpp
+//  ScientificProgramming
+//
+//  Created by Oscar Alejandro Hernández López on 06/07/20.
+//  Copyright © 2020 Oscar Alejandro Hernández López. All rights reserved.
+//
+
+#include "Header.h"
+
+//Include the files where the data will be read.
+#include "01_Area_Perimeter.hpp"
+#include "02_Capturing_Integers.hpp"
+#include "03_Calculate_Distance.hpp"
+#include "04_TypeIntegers.hpp"
+#include "05_SmallestValue.hpp"
+#include "06_Fibonacci.hpp"
+#include "07_Factorial.hpp"
+#include "08_PrimeNumber.hpp"
+#include "09_RandomNumber_Game.hpp"
+#include "Pointer_Table.hpp"
+#include "NK_Tables.hpp"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    
+    /*Call the stuff in files:*/
+    
+    //calculate_area_perimeter_rectangle();
+    //type_integers();
+    //calculate_distance();
+    //typeIntegers_while();
+    //smallestValue();
+    //fibonacci();
+    //factorial();
+    //isTheNumberPrime();
+    //randomNumber();
+    //mainPointerTable();
+    //mainNKTables();
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
