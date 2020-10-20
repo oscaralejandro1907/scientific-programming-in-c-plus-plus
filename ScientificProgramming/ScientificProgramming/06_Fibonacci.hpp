f6d671e0df7fba85d88ee5f98bc5613d64156dd6
Oscar Alejandro Hernandez Lopez <oscarale930719@gmail.com>
2020-10-20 06:18:10 -0500

    Create 06_Fibonacci.hpp

---
+++ ScientificProgramming/ScientificProgramming/06_Fibonacci.hpp
@@ -0,0 +1,17 @@
+//
+//  06_Fibonacci.hpp
+//  ScientificProgramming
+//
+//  Created by Oscar Alejandro Hernández López on 14/09/20.
+//  Copyright © 2020 Oscar Alejandro Hernández López. All rights reserved.
+//
+
+#ifndef _6_Fibonacci_hpp
+#define _6_Fibonacci_hpp
+
+#include <stdio.h>
+#include "Header.h"
+
+void fibonacci ();
+
+#endif /* _6_Fibonacci_hpp */
