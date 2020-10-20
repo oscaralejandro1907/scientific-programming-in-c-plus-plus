78d1971a6cbab035750a796e1c9d7a4435689c92
Oscar Alejandro Hernandez Lopez <oscarale930719@gmail.com>
2020-10-20 06:18:19 -0500

    Create 08_PrimeNumber.hpp

---
+++ ScientificProgramming/ScientificProgramming/08_PrimeNumber.hpp
@@ -0,0 +1,17 @@
+//
+//  08_PrimeNumber.hpp
+//  ScientificProgramming
+//
+//  Created by Oscar Alejandro Hernández López on 14/09/20.
+//  Copyright © 2020 Oscar Alejandro Hernández López. All rights reserved.
+//
+
+#ifndef _8_PrimeNumber_hpp
+#define _8_PrimeNumber_hpp
+
+#include <stdio.h>
+#include "Header.h"
+
+void isTheNumberPrime();
+
+#endif /* _8_PrimeNumber_hpp */
