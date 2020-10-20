2307f352356b12da10aa4bc03dc49c9e3127cc4c
Oscar Alejandro Hernandez Lopez <oscarale930719@gmail.com>
2020-10-20 06:17:51 -0500

    Create 03_Calculate_Distance.hpp

---
+++ ScientificProgramming/ScientificProgramming/03_Calculate_Distance.hpp
@@ -0,0 +1,17 @@
+//
+//  03_Calculate_Distance.hpp
+//  ScientificProgramming
+//
+//  Created by Oscar Alejandro Hernández López on 06/07/20.
+//  Copyright © 2020 Oscar Alejandro Hernández López. All rights reserved.
+//
+
+#ifndef _3_Calculate_Distance_hpp
+#define _3_Calculate_Distance_hpp
+
+#include <stdio.h>
+#include "Header.h"
+
+float calculate_distance ();
+
+#endif /* _3_Calculate_Distance_hpp */
