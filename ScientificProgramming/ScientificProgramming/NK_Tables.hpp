719a8478d5a53d1f0ffd03af3f7b15afd1677fd9
Oscar Alejandro Hernandez Lopez <oscarale930719@gmail.com>
2020-10-20 06:18:30 -0500

    Create NK_Tables.hpp

---
+++ ScientificProgramming/ScientificProgramming/NK_Tables.hpp
@@ -0,0 +1,19 @@
+//
+//  NK_Tables.hpp
+//  ScientificProgramming
+//
+//  Created by Oscar Alejandro Hernández López on 18/09/20.
+//  Copyright © 2020 Oscar Alejandro Hernández López. All rights reserved.
+//
+
+#ifndef NK_Tables_hpp
+#define NK_Tables_hpp
+
+#include <stdio.h>
+#include "Header.h"
+
+void mainNKTables();
+void fillRandomly(int quantityK,  int *PointerofLengths, float **PointerMatrix);
+void printTables(int quantityK, int *PointerofLenghts, float **PointerMatrix);
+
+#endif /* NK_Tables_hpp */
