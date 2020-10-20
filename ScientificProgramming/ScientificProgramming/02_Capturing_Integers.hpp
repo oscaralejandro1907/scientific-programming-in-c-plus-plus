9a1afe5fdc0094cba1fb70f204a1a8546e87e283
Oscar Alejandro Hernandez Lopez <oscarale930719@gmail.com>
2020-10-20 06:17:41 -0500

    Create 02_Capturing_Integers.hpp

---
+++ ScientificProgramming/ScientificProgramming/02_Capturing_Integers.hpp
@@ -0,0 +1,17 @@
+//
+//  02_Capturing_Integers.hpp
+//  ScientificProgramming
+//
+//  Created by Oscar Alejandro Hernández López on 06/07/20.
+//  Copyright © 2020 Oscar Alejandro Hernández López. All rights reserved.
+//
+
+#ifndef _2_Capturing_Integers_hpp
+#define _2_Capturing_Integers_hpp
+
+#include "Header.h"
+#include <stdio.h>
+
+void capturing_integers();
+
+#endif /* _2_Capturing_Integers_hpp */
