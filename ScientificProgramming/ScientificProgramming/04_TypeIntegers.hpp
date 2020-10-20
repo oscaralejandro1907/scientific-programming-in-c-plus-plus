17fb77b48d897a29e0630fc516f38904218854e0
Oscar Alejandro Hernandez Lopez <oscarale930719@gmail.com>
2020-10-20 06:17:56 -0500

    Create 04_TypeIntegers.hpp

---
+++ ScientificProgramming/ScientificProgramming/04_TypeIntegers.hpp
@@ -0,0 +1,19 @@
+//
+//  04_TypeIntegers.hpp
+//  ScientificProgramming
+//
+//  Created by Oscar Alejandro Hernández López on 09/07/20.
+//  Copyright © 2020 Oscar Alejandro Hernández López. All rights reserved.
+//
+
+#ifndef _4_TypeIntegers_hpp
+#define _4_TypeIntegers_hpp
+
+#include "Header.h"
+#include <stdio.h>
+
+void typeIntegers_for();
+
+void typeIntegers_while();
+
+#endif /* _4_TypeIntegers_hpp */
