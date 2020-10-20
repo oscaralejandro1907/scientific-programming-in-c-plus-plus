b51ddb9c684a4bfcb566562924dec461d728826e
Oscar Alejandro Hernandez Lopez <oscarale930719@gmail.com>
2020-10-20 06:18:03 -0500

    Create 05_SmallestValue.hpp

---
+++ ScientificProgramming/ScientificProgramming/05_SmallestValue.hpp
@@ -0,0 +1,17 @@
+//
+//  05_SmallestValue.hpp
+//  ScientificProgramming
+//
+//  Created by Oscar Alejandro Hernández López on 09/07/20.
+//  Copyright © 2020 Oscar Alejandro Hernández López. All rights reserved.
+//
+
+#ifndef _5_SmallestValue_hpp
+#define _5_SmallestValue_hpp
+
+#include <stdio.h>
+#include "Header.h"
+
+int smallestValue();
+
+#endif /* _5_SmallestValue_hpp */
