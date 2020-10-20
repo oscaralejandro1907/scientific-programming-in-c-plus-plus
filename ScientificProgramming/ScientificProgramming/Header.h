bca39de07c7c2f9e0d753f60fc9eb53f0f888a15
Oscar Alejandro Hernandez Lopez <oscarale930719@gmail.com>
2020-10-20 06:18:44 -0500

    Create Header.h

---
+++ ScientificProgramming/ScientificProgramming/Header.h
@@ -0,0 +1,17 @@
+//
+//  Header.h
+//  ScientificProgramming
+//
+//  Created by Oscar Alejandro Hernández López on 06/07/20.
+//  Copyright © 2020 Oscar Alejandro Hernández López. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+#endif /* Header_h */
