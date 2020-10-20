fea14f35fcf28c90d4e7e66275263261bbca88d1
Oscar Alejandro Hernandez Lopez <oscarale930719@gmail.com>
2020-10-20 06:17:35 -0500

    Create 01_Area_Perimeter.hpp

---
+++ ScientificProgramming/ScientificProgramming/01_Area_Perimeter.hpp
@@ -0,0 +1,21 @@
+//
+//  01_Area_Perimeter.hpp
+//  ScientificProgramming
+//
+//  Created by Oscar Alejandro Hernández López on 06/07/20.
+//  Copyright © 2020 Oscar Alejandro Hernández López. All rights reserved.
+//
+
+#ifndef _1_Area_Perimeter_hpp
+#define _1_Area_Perimeter_hpp
+
+#include <stdio.h>
+#include "Header.h"
+
+//1. Aquí está declarado el prototipo de función, indicando al programa que usará una función que nosotros creamos y no una que las librerías tienen predefinidas.
+//2. También declaramos que vamos a incluir el Header.h arriba ya que se utilizarán todas las librerías del proyecto que se utilizarán.
+//3. Ese hpp también debemos incluirlo en el main para que ejecute lo que está aqui, en este caso la función
+
+void calculate_area_perimeter_rectangle ();
+
+#endif /* _1_Area_Perimeter_hpp */
