5bfe4f2bdca26d3fff494ae9c08e4b17f5e385be
Oscar Alejandro Hernandez Lopez <oscarale930719@gmail.com>
2020-11-03 18:00:32 -0600

    Create Header.h

---
+++ Tests/Tests/Header.h
@@ -0,0 +1,15 @@
+//
+//  Header.h
+//  Tests
+//
+//  Created by Oscar Alejandro Hernández López on 03/11/20.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+#endif /* Header_h */
