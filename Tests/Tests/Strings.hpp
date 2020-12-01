73805fdb5e2690c65eed8c3e1646196a42bd4d66
Oscar Alejandro Hernandez Lopez <oscarale930719@gmail.com>
2020-12-01 08:58:42 -0600

    Create Strings.hpp

---
+++ Tests/Tests/Strings.hpp
@@ -0,0 +1,13 @@
+//
+//  Strings.hpp
+//  Tests
+//
+//  Created by Oscar Alejandro Hernández López on 22/11/20.
+//
+
+#ifndef Strings_hpp
+#define Strings_hpp
+
+#include <stdio.h>
+
+#endif /* Strings_hpp */
