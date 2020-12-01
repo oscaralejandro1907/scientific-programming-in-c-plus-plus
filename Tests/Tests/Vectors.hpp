4437140b7844a97ccb6c582d06b2841eda1f4b4f
Oscar Alejandro Hernandez Lopez <oscarale930719@gmail.com>
2020-12-01 09:17:40 -0600

    Create Vectors.hpp

---
+++ Tests/Tests/Vectors.hpp
@@ -0,0 +1,39 @@
+//
+//  Vectors.hpp
+//  Tests
+//
+//  Created by Oscar Alejandro Hernández López on 25/11/20.
+//
+
+#ifndef Vectors_hpp
+#define Vectors_hpp
+
+#include <stdio.h>
+#include "Header.h"
+
+class Vectors{
+private:
+    
+    vector<int> _myvec1;
+    vector<int> _myvec2;
+    vector<int> _myvec3;
+    
+public:
+    Vectors();
+    
+    vector<int> getVector1() {return _myvec1;};
+    
+    int sum(vector<int> vec);
+    void appendOtherVector();
+    bool isAlreadyAdded(int value, int index);
+    void addRandomElements();
+    void printAnIntVector(vector<int> vec);
+    void sortVector(vector<int> vec);
+    int getGreaterValue(vector<int> vec);
+    int getMinValue(vector<int> vec);
+    int searchIndex(vector<int> vec, int key);
+    
+    void average();
+};
+
+#endif /* Vectors_hpp */
