35d9184736c380b402876a5d5f16be6636275aca
Oscar Alejandro Hernandez Lopez <oscarale930719@gmail.com>
2020-12-01 08:56:57 -0600

    Create Header.h

---
+++ cplex/cplex/Header.h
@@ -0,0 +1,16 @@
+//
+//  Header.h
+//  cplex
+//
+//  Created by Oscar Alejandro Hernández López on 26/11/20.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#include <iostream>
+
+using namespace std;
+
+
+#endif /* Header_h */
