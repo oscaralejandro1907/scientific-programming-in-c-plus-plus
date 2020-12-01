f449d89ac4b3a61b852b4a26a64f61cacc576518
Oscar Alejandro Hernandez Lopez <oscarale930719@gmail.com>
2020-12-01 08:58:21 -0600

    Create Maths.hpp

---
+++ Tests/Tests/Maths.hpp
@@ -0,0 +1,48 @@
+//
+//  Maths.hpp
+//  Tests
+//
+//  Created by Oscar Alejandro Hernández López on 21/11/20.
+//
+
+#ifndef Maths_hpp
+#define Maths_hpp
+
+#include <stdio.h>
+#include "Header.h"
+#include "Instance.hpp"
+
+class Maths {
+private:
+    
+    float _base;
+    float _height;
+    
+    float _a,_b,_c;
+    
+public:
+    Maths();
+    ~Maths();
+    
+    float formula();
+    float areaTriangle();
+    int sumofN(int N);
+    void roots();
+    float areaCircle(int radio);
+    float cylinderVolume(int radio, int height);
+    void arithmeticOperations(float a, float b, int option);
+    void multiplicationTable(int n);
+    
+    long int factorial(int N);
+    vector<int> findingFactors(int n);
+    
+    void isPerfectNumber(int anumber);
+    void isPrimeNumber(int n);
+    
+    void generateRandomNumber();
+    
+    
+    
+};
+
+#endif /* Maths_hpp */
